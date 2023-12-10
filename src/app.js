dbf62c9d68707b07eee6f27dbefb84754ba0b17f
Samir Khoury <samir@khoury.tk>
2023-12-10 15:28:49 +0200

    removed unused imports

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import 'express-async-errors';
-// import 'module-alias/register.js';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import globalErrorHandler from './errors/globalErrorHandler.js';
 import catchProgrammingErrors from './errors/catchProgrammingErrors.js';
 import AppError from './errors/AppError.js';
 import corsMiddleware from './middlewares/cors.js';
+
 //import routes
-// import userRoutes from './apps/users/entryPoints/userRoutes.js';
 import messagesRoutes from './apps/messages/entryPoints/messageRoutes.js';
 import categoriesRoutes from './apps/categories/entryPoints/categoryRoutes.js';
 
@@ -17,14 +16,13 @@ const app = express();
 app.use(cookieParser());
 app.use(corsMiddleware);
 app.use(express.json());
-//route logger
+//route console logger
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
 });
 
 // Routes
-// app.use('/users', userRoutes); //? route not used currently
 app.use('/api/messages', messagesRoutes);
 app.use('/api/categories', categoriesRoutes);
 
