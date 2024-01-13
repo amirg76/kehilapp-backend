b45122c17b2bad24eee83d070da3cccfa2e6dc8f
Samir Khoury <samir@khoury.tk>
2024-01-13 23:27:39 +0200

    updated messages database service functions

--- src/apps/messages/dataAccess/messageRepository.js
+++ src/apps/messages/dataAccess/messageRepository.js
@@ -1,36 +1,40 @@
 import MessageModel from './messageModel.js';
 
-// TODO: filtering dynamically by field - mongo aggregation framework
-export const getMessagesFromDb = async (filterBy) => {
-  return await MessageModel.find(filterBy).sort({ createdAt: -1 }); //sorted as created last shown first
+export const getMessagesFromDb = async () => {
+  return await MessageModel.find({}).sort({ createdAt: -1 }).lean(); //sorted as created last shown first
 };
 
 export const getMessageByCategoryFromDb = async (categoryId) => {
-  return await MessageModel.find({ categoryId }).sort({ createdAt: -1 });
+  return await MessageModel.find({ categoryId }).sort({ createdAt: -1 }).lean();
 };
 
 //TODO: change logic to fit the "ראשי" category, currently return the 5 newest messages
 export const getLatestMessagesFromDb = async () => {
-  return await MessageModel.find({}).sort({ createdAt: -1 }).limit(5);
+  return await MessageModel.find({}).sort({ createdAt: -1 }).limit(5).lean();
 };
 
 export const getMessagesByQueryFromDb = async (stringRegex) => {
   const query = {
     $or: [{ title: { $regex: stringRegex } }, { text: { $regex: stringRegex } }],
   };
-  return await MessageModel.find(query).sort({ createdAt: -1 }).limit(5);
+  return await MessageModel.find(query).sort({ createdAt: -1 }).limit(5).lean();
 };
 
 export const getMessageByIdFromDb = async (id) => {
-  return await MessageModel.findById(id);
+  return await MessageModel.findById(id).lean();
 };
 
-export const addMessageToDb = async (message) => {
-  return await MessageModel.create(message);
+//TODO: get user id from auth token and add to db
+export const addMessageToDb = async (categoryId, title, text, attachmentName, attachmentKey, attachmentType) => {
+  return await MessageModel.create({ categoryId, title, text, attachmentName, attachmentKey, attachmentType });
 };
 
-export const updateMessageInDb = async (id, updatedMessage) => {
-  return await MessageModel.findByIdAndUpdate(id, updatedMessage, { new: true });
+export const updateMessageInDb = async (id, categoryId, title, text, attachmentName, attachmentKey, attachmentType) => {
+  return await MessageModel.findByIdAndUpdate(
+    id,
+    { categoryId, title, text, attachmentName, attachmentKey, attachmentType },
+    { new: true },
+  );
 };
 
 export const deleteMessageInDb = async (id) => {
