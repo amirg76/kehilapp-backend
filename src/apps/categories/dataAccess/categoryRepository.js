120fae5a70e1d64e348ad9d1591147158ef3acd8
Samir Khoury <samir@khoury.tk>
2024-02-29 21:13:31 +0200

    updated repository function to have clear variables

--- src/apps/categories/dataAccess/categoryRepository.js
+++ src/apps/categories/dataAccess/categoryRepository.js
@@ -1,25 +1,21 @@
 import CategoryModel from './categoryModel.js';
 
-export const getCategoriesFromDb = async (filterBy) => {
-  return await CategoryModel.find({}).sort({ createdAt: 1 }); //sorted as created first shown first
+export const getCategoriesFromDb = async () => {
+  return await CategoryModel.find({}).sort({ createdAt: 1 }).lean(); //sorted as created first shown first
 };
 
 export const getCategoryByIdFromDb = async (id) => {
-  return await CategoryModel.findById(id);
+  return await CategoryModel.findById(id).lean();
 };
 
-export const addCategoryToDb = async (category) => {
-  return await CategoryModel.create(category);
+export const addCategoryToDb = async (title, managedBy, icon, categoryColor, attachmentKey) => {
+  return await CategoryModel.create({ title, managedBy, icon, categoryColor, attachmentKey });
 };
 
-export const updateCategoryInDb = async (id, updatedCategory) => {
-  return await CategoryModel.findByIdAndUpdate(id, updatedCategory, { new: true });
+export const updateCategoryInDb = async (id, title, managedBy, icon, categoryColor) => {
+  return await CategoryModel.findByIdAndUpdate(id, { title, managedBy, icon, categoryColor }, { new: true });
 };
 
 export const deleteCategoryInDb = async (id) => {
   return await CategoryModel.findByIdAndDelete(id);
-};
-
-export const addCategoryImgInDb = async (categoryId, attachmentKey) => {
-  return await CategoryModel.findByIdAndUpdate(categoryId, { attachmentKey }, { new: true });
 };
