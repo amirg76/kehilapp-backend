fe1d2c349619589d46235b54042724f38a8c5ff9
amirg76 <amirg76@gmail.com>
2024-09-10 12:02:19 +0300

    refactor error global handel and register error handel

--- src/errors/globalErrorHandler.js
+++ src/errors/globalErrorHandler.js
@@ -1,5 +1,6 @@
 import errorManagement from './utils/errorManagement.js'; // Update path as needed
 import logger from '../services/logger.js';
+import AppError from './AppError.js';
 
 const globalErrorHandler = (error, req, res, next) => {
   const statusCode = error.statusCode || errorManagement.commonErrors.internalServerError.code;
@@ -13,11 +14,19 @@ const globalErrorHandler = (error, req, res, next) => {
   //   mailer.sendMail(configuration.adminMail, "Critical error occurred", error);
   // }
 
-  if (!error.isOperational) {
-    process.exit(1);
-  }
+  // if (!error.isOperational) {
+  //   process.exit(1);
+  // }
+  const isOperationalError = error instanceof AppError;
+  const errorMessage = isOperationalError ? error.message : 'An unexpected error occurred';
 
-  res.status(statusCode).json({ error: message });
+  res.status(200).json({
+    success: false,
+    error: {
+      status: error.statusCode || 500,
+      message: errorMessage,
+    },
+  });
 };
 
 export default globalErrorHandler;
