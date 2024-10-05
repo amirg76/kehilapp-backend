f2da84cf98c0b7dfb388cfeb9631d2678297bd24
amirg76 <amirg76@gmail.com>
2024-10-05 16:08:51 +0300

    add phone field and timestamp for user schema

--- src/apps/users/dataAccess/userModel.js
+++ src/apps/users/dataAccess/userModel.js
@@ -1,19 +1,23 @@
 import { applyErrorHandlingMiddleware } from '../../../errors/utils/dbErrorHandling.js';
 import mongoose, { Schema } from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  // username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  // Regular user fields
-  firstName: String,
-  lastName: String,
-  // Admin-specific fields
-  adminLevel: { type: Number, default: 0 },
-  adminPermissions: [String],
-  lastAdminAction: Date,
-});
+const userSchema = new mongoose.Schema(
+  {
+    // username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    // Regular user fields
+    firstName: String,
+    lastName: String,
+    phone: String,
+    // Admin-specific fields
+    adminLevel: { type: Number, default: 0 },
+    adminPermissions: [String],
+    lastAdminAction: Date,
+  },
+  { timestamps: true },
+);
 
 // Apply error handling middleware
 applyErrorHandlingMiddleware(userSchema);
